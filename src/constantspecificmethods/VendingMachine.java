83486a752b6a8155c4051d2b66fb08d6929fd8d9
oleh27yo <baibula.oo@gmail.com>
2023-02-14 11:31:33 +0200

    comm

--- src/constantspecificmethods/VendingMachine.java
+++ src/constantspecificmethods/VendingMachine.java
@@ -1,10 +1,13 @@
 package constantspecificmethods;
 
-import java.util.*;
 import java.io.IOException;
-import java.util.function.*;
-import java.nio.file.*;
-import java.util.stream.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Iterator;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 enum Category {
     MONEY(Input.NICKEL, Input.DIME,
@@ -13,15 +16,21 @@ enum Category {
             Input.SODA, Input.SOAP),
     QUIT_TRANSACTION(Input.ABORT_TRANSACTION),
     SHUT_DOWN(Input.STOP);
-    private Input[] values;
-    Category(Input... types) { values = types; }
-    private static EnumMap<Input,Category> categories =
+    private static EnumMap<Input, Category> categories =
             new EnumMap<>(Input.class);
+
     static {
-        for(Category c : Category.class.getEnumConstants())
-            for(Input type : c.values)
+        for (Category c : Category.class.getEnumConstants())
+            for (Input type : c.values)
                 categories.put(type, c);
     }
+
+    private Input[] values;
+
+    Category(Input... types) {
+        values = types;
+    }
+
     public static Category categorize(Input input) {
         return categories.get(input);
     }
@@ -31,11 +40,30 @@ public class VendingMachine {
     private static State state = State.RESTING;
     private static int amount = 0;
     private static Input selection = null;
-    enum StateDuration { TRANSIENT } // Tagging enum
+
+    static void run(Supplier<Input> gen) {
+        while (state != State.TERMINAL) {
+            state.next(gen.get());
+            while (state.isTransient)
+                state.next();
+            state.output();
+        }
+    }
+
+    public static void main(String[] args) {
+        Supplier<Input> gen = new RandomInputSupplier();
+        if (args.length == 1)
+            gen = new FileInputSupplier(args[0]);
+        run(gen);
+    }
+
+    enum StateDuration {TRANSIENT} // Tagging enum
+
     enum State {
         RESTING {
-            @Override void next(Input input) {
-                switch(Category.categorize(input)) {
+            @Override
+            void next(Input input) {
+                switch (Category.categorize(input)) {
                     case MONEY:
                         amount += input.amount();
                         state = ADDING_MONEY;
@@ -47,14 +75,15 @@ public class VendingMachine {
             }
         },
         ADDING_MONEY {
-            @Override void next(Input input) {
-                switch(Category.categorize(input)) {
+            @Override
+            void next(Input input) {
+                switch (Category.categorize(input)) {
                     case MONEY:
                         amount += input.amount();
                         break;
                     case ITEM_SELECTION:
                         selection = input;
-                        if(amount < selection.amount())
+                        if (amount < selection.amount())
                             System.out.println(
                                     "Insufficient money for " + selection);
                         else state = DISPENSING;
@@ -69,56 +98,59 @@ public class VendingMachine {
             }
         },
         DISPENSING(StateDuration.TRANSIENT) {
-            @Override void next() {
+            @Override
+            void next() {
                 System.out.println("here is your " + selection);
                 amount -= selection.amount();
                 state = GIVING_CHANGE;
             }
         },
         GIVING_CHANGE(StateDuration.TRANSIENT) {
-            @Override void next() {
-                if(amount > 0) {
+            @Override
+            void next() {
+                if (amount > 0) {
                     System.out.println("Your change: " + amount);
                     amount = 0;
                 }
                 state = RESTING;
             }
         },
-        TERMINAL {@Override
-        void output() { System.out.println("Halted"); } };
+        TERMINAL {
+            @Override
+            void output() {
+                System.out.println("Halted");
+            }
+        };
         private boolean isTransient = false;
-        State() {}
-        State(StateDuration trans) { isTransient = true; }
+
+        State() {
+        }
+
+        State(StateDuration trans) {
+            isTransient = true;
+        }
+
         void next(Input input) {
             throw new RuntimeException("Only call " +
                     "next(Input input) for non-transient states");
         }
+
         void next() {
             throw new RuntimeException(
                     "Only call next() for " +
                             "StateDuration.TRANSIENT states");
         }
-        void output() { System.out.println(amount); }
-    }
-    static void run(Supplier<Input> gen) {
-        while(state != State.TERMINAL) {
-            state.next(gen.get());
-            while(state.isTransient)
-                state.next();
-            state.output();
+
+        void output() {
+            System.out.println(amount);
         }
-    }
-    public static void main(String[] args) {
-        Supplier<Input> gen = new RandomInputSupplier();
-        if(args.length == 1)
-            gen = new FileInputSupplier(args[0]);
-        run(gen);
     }
 }
 
 // For a basic sanity check:
 class RandomInputSupplier implements Supplier<Input> {
-    @Override public Input get() {
+    @Override
+    public Input get() {
         return Input.randomSelection();
     }
 }
@@ -126,6 +158,7 @@ class RandomInputSupplier implements Supplier<Input> {
 // Create Inputs from a file of ';'-separated strings:
 class FileInputSupplier implements Supplier<Input> {
     private Iterator<String> input;
+
     FileInputSupplier(String fileName) {
         try {
             input = Files.lines(Paths.get(fileName))
@@ -134,12 +167,14 @@ class FileInputSupplier implements Supplier<Input> {
                     .map(String::trim)
                     .collect(Collectors.toList())
                     .iterator();
-        } catch(IOException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    @Override public Input get() {
-        if(!input.hasNext())
+
+    @Override
+    public Input get() {
+        if (!input.hasNext())
             return null;
         return Enum.valueOf(
                 Input.class, input.next().trim());
